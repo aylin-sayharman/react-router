be9a4e9afcd69331eb12829e70b69122af507e8b
Aylin Sayharman <aylin.sayharman@gmail.com>
2021-11-13 16:02:21 +0300

    add dynamic route

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Products = () => {
-  return <h1>The products page</h1>
+  return (
+    <section>
+      <h1>The products page</h1>
+      <ul>
+        <li><Link to='products/p1'>Product 1</Link></li>
+        <li><Link to='products/p2'>Product hg</Link></li>
+        <li><Link to='products/p3'>Product kjhcc</Link></li>
+      </ul>
+    </section >
+  );
 };
 
 export default Products;
